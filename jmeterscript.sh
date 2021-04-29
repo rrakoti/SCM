a5230dbc108eaed99b9789c809051074a9a84bdc
rrakoti <83219405+rrakoti@users.noreply.github.com>
2021-04-29 12:29:27 +0530

    Add files via upload

--- jmeterscript.sh
+++ jmeterscript.sh
@@ -6,4 +6,4 @@ echo $JAVA_HOME;
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64;
 echo $JAVA_HOME;
 cd apache-jmeter-5.0/bin;
-./jmeter -n -t /var/lib/jenkins/workspace/jmetertest1/SCM.jmx -l /var/lib/jenkins/workspace/jmetertest1/result1.csv;
+./jmeter -n -t /var/lib/jenkins/workspace/jmetertest2/SCM.jmx -l /var/lib/jenkins/workspace/jmetertest2/result1.csv;
